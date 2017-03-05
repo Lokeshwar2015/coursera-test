7a2bd36f09699eb3a57a6a4c9a386f84bd6607c8
LOKESHWAR MANOHARAN <LOKESHWAR MANOHARAN>
2017-03-04 22:57:38 -0800

    angular added

---
+++ controller/signInController.js
@@ -0,0 +1,19 @@
+/**
+ * Created by LOKESHWAR on 3/4/2017.
+ */
+
+(function () {
+
+    angular.module("signin")
+
+        .controller("signInController",
+        function($scope)
+        {
+            $scope.welcomeMessage = "Hello Lokesh";
+        }
+
+
+
+        )
+
+})()
---
+++ module/signInModule.js
@@ -0,0 +1,10 @@
+/**
+ * Created by LOKESHWAR on 3/4/2017.
+ */
+
+(function () {
+
+    angular.module("signin",[]);
+
+
+})()
